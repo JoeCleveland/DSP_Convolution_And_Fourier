2dcc7bb60e33da4c2864bd5dbe4e9a43327f4386
Joseph Cleveland <josephcleveland@lawn-128-61-8-254.lawn.gatech.edu>
2025-01-26 15:10:28 -0500

    random sequence

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def random_sequence():
-    a = np.random.uniform(10000)
-    b = np.random.uniform(10000)
+    a = np.random.uniform(low=-1, high=1, size=10000)
+    b = np.random.uniform(low=-1, high=1, size=10000)
+    y = a * b
+
+    hist_a, bins_a = np.histogram(a, 100)
+    hist_b, bins_b = np.histogram(b, 100)
+    hist_y, bins_y = np.histogram(y, 100)
+
+    center_a = (bins_a[:-1] + bins_a[1:]) * 0.5
+    pmf_a = hist_a / np.sum(hist_a)
+    plt.bar(center_a, pmf_a, width=(bins_a[1] - bins_a[0]), alpha=0.7)
+    plt.show()
+    # plt.bar(hist_b)
+
+    plt.savefig("hist_a_b.svg")
+
+    # plt.bar(hist_y)
+
+    # plt.savefig("hist_y.svg")
+
+random_sequence()
