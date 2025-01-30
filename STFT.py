20af5f46da35437d211e4f0db0ab68cbbf9ea21f
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2025-01-30 14:59:45 -0500

    added some comment on stft

--- STFT.py
+++ STFT.py
@@ -7,13 +7,17 @@ import matplotlib.pyplot as plt
 def STFT(input, window_fn, window_size, hop_size):
     win = window_fn(window_size)
     input_len = input.shape[0]
+    # [1, 2, 3, ...]
 
     end_padding = window_size - (input_len % hop_size)
+
+    # [1, 2, 3, ..., 0, 0]
     signal = np.concatenate((input, np.zeros(end_padding)))
     signal_len = signal.shape[0]
 
-    n_hops = signal_len // hop_size - 1
+    n_hops = signal_len // hop_size - 1  # (signal_len / hop_size) - 1
 
+    # create a placehodler matrix of size [n_hops x (window_size/2)]
     stft_output = np.zeros((n_hops, window_size // 2))
 
     for n in range(n_hops):
@@ -32,6 +36,3 @@ stft_out = STFT(signal, windows.blackman, 2048, 1024)
 
 plt.imshow(stft_out)
 plt.show()
-
-
-
